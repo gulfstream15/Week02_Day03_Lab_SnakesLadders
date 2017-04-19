c5057582500ed1dd76c4a81aa81fb47794af1195
User <user@Users-MacBook-Pro.local>
2017-04-19 11:41:58 +0100

    Adding final position

--- board.rb
+++ board.rb
@@ -4,10 +4,23 @@ class Board
 
   def initialize()
       @board_array = []
+      @final_position = 20
   end
 
-  def can_check_array
-    @board_array = [1, 2, 3, 4]
+  def can_check_array()
+    @board_array = [1..20]
+    # puts "board_array is #{@board_array}"
+    return @board_array
+  end
+
+  def can_add_to_board()
+    @board_array[19] = "mike"
+    puts "board_array is #{@board_array}"
+    return @board_array[19]
+  end
+
+  def get_final_position
+    return @final_position
   end
 
 end
--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -10,8 +10,18 @@ class BoardTest < MiniTest::Test
   end  
 
   def test_check_array()
-     board = Board.new()
-     assert_equal([1, 2, 3, 4], board.can_check_array)
+    board = Board.new()
+    assert_equal([1..20], board.can_check_array)
   end
+
+  def test_can_add_to_board_array
+    board = Board.new()
+    assert_equal("mike", board.can_add_to_board)
+  end
+   
+  def test_get_final_position
+    board = Board.new()
+    assert_equal(20, board.get_final_position) 
+  end 
 
 end
